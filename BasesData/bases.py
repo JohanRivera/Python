0441ad148b33a3ffe47fbacd47f5227251070827
JohanRivera <46936157+JohanRivera@users.noreply.github.com>
2020-07-18 14:35:42 -0500

    BaseData

---
+++ BasesData/bases.py
@@ -0,0 +1,9 @@
+# ----------------------------------------- MODELS OF DATA BASES ---------------------------------------------
+"""
+The model of data bases is a group of logic ideas uses to represent the structure of the DBs, between there
+models is:
+    *Relational model: It allows to see the structure of a DB and relations between the datas. For this, is
+        necesary to use a tool that allow us make the model, and this tool is:
+            *Entity relation diagram: It allows to make the model of our DB and analize it.
+                To see the elements in a Entity relation diagram, see the image in this binder.
+"""
