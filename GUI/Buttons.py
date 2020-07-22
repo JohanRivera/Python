6d4e089990228c1bf09c984de2f8c83f950a2dd4
JohanRivera <46936157+JohanRivera@users.noreply.github.com>
2020-07-22 14:29:02 -0500

    Buttons and textBox

--- GUI/Buttons.py
+++ GUI/Buttons.py
@@ -2,8 +2,10 @@ from tkinter import *
 raiz = Tk()
 myFrame = Frame(raiz, width=800, height=400)
 myFrame.pack()
+myName = StringVar()
+option = IntVar()
 
-textBox = Entry(myFrame)
+textBox = Entry(myFrame, text=myName)
 textBox.grid(row=0,column=1) #grid is for controlate all using rows and columns 
 #Further, in .grid(row=0, column=1, sticky='e', padx=50) the comand padx o pady move the object, in this case
 #50 pixels to este, this direction can changed with the comand sticky
@@ -11,20 +13,24 @@ textBox.grid(row=0,column=1) #grid is for controlate all using rows and columns
 passBox = Entry(myFrame)
 passBox.grid(row=1,column=1)
 passBox.config(show='*')#this line is for changed the characters with a character special
-""" Text Box to comments"""
-commentsBox = Text(myFrame,width=10,height=5) #Form to create the comment box
-commentsBox.grid(row=2,column=1) 
-scrollVertical = Scrollbar(myFrame,command=commentsBox.yview) # Form to make a scroll in the comment box 
-scrollVertical.grid(row=2,column=2,sticky='nsew') # the command nsew is what the scroll it have the same size
-#that comment box
-commentsBox.config(yscrollcommand=scrollVertical.set) #For the scroll 
-
 
 myLabel = Label(myFrame, text = 'Ingrese el nombre: ')
 myLabel.grid(row=0,column=0) #grid is for controlate all using rows and columns 
 myPass = Label(myFrame, text = 'Ingrese contraseña: ')
 myPass.grid(row=1,column=0)
-myComments = Label(myFrame, text = 'Comentarios: ')
-myComments.grid(row=2,column=0)
+
+def codeButton():
+    if option.get() == 1: # set or get
+       labelPrint.config(text = 'Bienvenido ' + textBox.get()) 
+       labelPrint.pack()
+    else: 
+        labelPrint.config(text = 'Bienvenida ' + textBox.get())
+        labelPrint.pack()
+
+labelPrint = Label(raiz)
+Radiobutton(raiz, text='Male', variable=option, value=1).pack() #Create an option to select
+Radiobutton(raiz, text='Female', variable=option, value=2).pack() #Create an option to select
+buttonSend = Button(raiz, text='Enviar', command = codeButton) #Create a button
+buttonSend.pack()
 
 raiz.mainloop()
